9a08a25054a392a943bd8ae842e164d5396d76a6
dorrl <dordorrl78@gmail.com>
2026-08-08 20:50:15 +0900

    8/8 빌드 테스트 준비

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,6 @@ module.exports = function (api) {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            ['@babel/plugin-transform-typescript', { isTSX: true }],
-            '@babel/plugin-transform-class-properties',
-            '@babel/plugin-transform-private-methods',
-            '@babel/plugin-transform-private-property-in-object',
             [
                 'module-resolver',
                 {
